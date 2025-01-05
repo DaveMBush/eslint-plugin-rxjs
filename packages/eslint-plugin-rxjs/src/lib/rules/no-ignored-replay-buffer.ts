abca63c4763f942dea5a7abf79e1d3ea49f191f0
Dave Bush <dave@davembush.com>
2025-01-05 17:07:12 -0500

    fix bug with no-ignored-replay-buffer

--- packages/eslint-plugin-rxjs/src/lib/rules/no-ignored-replay-buffer.ts
+++ packages/eslint-plugin-rxjs/src/lib/rules/no-ignored-replay-buffer.ts
@@ -1,5 +1,4 @@
-import { TSESTree as es } from '@typescript-eslint/utils';
-import { getParent } from '../eslint-etc';
+import { AST_NODE_TYPES, TSESTree as es } from '@typescript-eslint/utils';
 
 import { ESLintUtils } from '@typescript-eslint/utils';
 
@@ -21,15 +20,41 @@ export default ESLintUtils.RuleCreator(() => __filename)({
   name: 'no-ignored-replay-buffer',
   defaultOptions: [],
   create: (context) => {
+    function checkShareReplayConfig(
+      node: es.Identifier,
+      shareReplayConfigArg: es.ObjectExpression,
+    ) {
+      if (
+        !shareReplayConfigArg.properties.some(
+          (p) =>
+            p.type === AST_NODE_TYPES.Property &&
+            p.key.type === AST_NODE_TYPES.Identifier &&
+            p.key.name === 'bufferSize',
+        )
+      ) {
+        context.report({
+          messageId: 'forbidden',
+          node,
+        });
+      }
+    }
+
     function checkNode(
-      node: es.Node,
-      { arguments: args }: { arguments: es.Node[] },
+      node: es.Identifier,
+      { arguments: args }: es.NewExpression | es.CallExpression,
     ) {
       if (!args || args.length === 0) {
         context.report({
-          messageId,
+          messageId: 'forbidden',
           node,
         });
+      }
+
+      if (node.name === 'shareReplay' && args?.length === 1) {
+        const arg = args[0];
+        if (arg.type === AST_NODE_TYPES.ObjectExpression) {
+          checkShareReplayConfig(node, arg);
+        }
       }
     }
 
@@ -37,22 +62,28 @@ export default ESLintUtils.RuleCreator(() => __filename)({
       "NewExpression > Identifier[name='ReplaySubject']": (
         node: es.Identifier,
       ) => {
-        const newExpression = getParent(node) as es.NewExpression;
+        const newExpression = node.parent as es.NewExpression;
         checkNode(node, newExpression);
       },
       "NewExpression > MemberExpression > Identifier[name='ReplaySubject']": (
         node: es.Identifier,
       ) => {
-        const memberExpression = getParent(node) as es.MemberExpression;
-        const newExpression = getParent(memberExpression) as es.NewExpression;
+        const memberExpression = node.parent as es.MemberExpression;
+        const newExpression = memberExpression.parent as es.NewExpression;
         checkNode(node, newExpression);
       },
       'CallExpression > Identifier[name=/^(publishReplay|shareReplay)$/]': (
         node: es.Identifier,
       ) => {
-        const callExpression = getParent(node) as es.CallExpression;
+        const callExpression = node.parent as es.CallExpression;
         checkNode(node, callExpression);
       },
+      'CallExpression > MemberExpression > Identifier[name=/^(publishReplay|shareReplay)$/]':
+        (node: es.Identifier) => {
+          const memberExpression = node.parent as es.MemberExpression;
+          const callExpression = memberExpression.parent as es.CallExpression;
+          checkNode(node, callExpression);
+        },
     };
   },
 });
